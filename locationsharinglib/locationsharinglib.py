66b18342ccf606ca90a3aaa539d8bcd5d6f3bb2e
Costas Tyfoxylos <costas.tyf@gmail.com>
2019-07-07 15:39:40 +0300

    fixes https://github.com/costastf/locationsharinglib/issues/62

--- locationsharinglib/locationsharinglib.py
+++ locationsharinglib/locationsharinglib.py
@@ -232,11 +232,11 @@ class Person:  # pylint: disable=too-many-instance-attributes
             self._country_code = data[1][6]
             try:
                 self._charging = data[13][0]
-            except TypeError:
+            except (IndexError, TypeError):
                 self._charging = None
             try:
                 self._battery_level = data[13][1]
-            except TypeError:
+            except (IndexError, TypeError):
                 self._battery_level = None
         except (IndexError, TypeError):
             self._logger.debug(data)
